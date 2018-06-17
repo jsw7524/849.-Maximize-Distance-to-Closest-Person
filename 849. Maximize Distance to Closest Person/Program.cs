203fddeac5058db6f6f134cbc8aa80e076674f2b
jsw7524 <jsw7524@gmail.com>
2018-06-17 16:40:55 +0800

    Add project files.

---
+++ 849. Maximize Distance to Closest Person/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _849.Maximize_Distance_to_Closest_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
