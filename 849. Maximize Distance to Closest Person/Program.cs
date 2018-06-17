76c63f6c29b7a50489df45edfa31091e2c646632
jsw7524 <jsw7524@gmail.com>
2018-06-17 19:29:01 +0800

    AC

--- 849. Maximize Distance to Closest Person/Program.cs
+++ 849. Maximize Distance to Closest Person/Program.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _849.Maximize_Distance_to_Closest_Person
 {
+
+    public class Solution
+    {
+        public int MaxDistToClosest(int[] seats)
+        {
+            //Case1
+            int l1 = 0;
+            while (1 != seats[l1]) l1++;
+            //Case2
+            int r1 = seats.Length-1;
+            while (1 != seats[r1]) r1--;
+            //Case3
+            int maxDistance = 0;
+            int tmp0=0;
+            for (int i=l1+1;i<=r1;i++)
+            {
+                if (0==seats[i])
+                {
+                    tmp0++;
+                }
+                else
+                {
+                    int ceilingTmp0 = (int)Math.Ceiling(tmp0 / 2.0);
+                    maxDistance = maxDistance > ceilingTmp0 ? maxDistance : ceilingTmp0;
+                    tmp0 = 0;
+                }
+            }
+            //MaxDistance
+            return Math.Max(Math.Max(l1, (seats.Length - 1) - r1), maxDistance);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using _849.Maximize_Distance_to_Closest_Person;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+            int result = sln.MaxDistToClosest(new int[] { 1, 0, 0, 0, 1, 0, 1 });
+            Assert.AreEqual(result, 2);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+            int result = sln.MaxDistToClosest(new int[] { 1, 0, 0, 0 });
+            Assert.AreEqual(result, 3);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution sln = new Solution();
+            int result = sln.MaxDistToClosest(new int[] { 1, 0, 0, 1, 0, 1, 0, 1 });
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
